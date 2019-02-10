d7387674ed99ecbe68fe34a3ca53ba8ff5fd365b
yinmo-public <yinmo.contact@gmail.com>
2019-02-10 15:36:35 +0800

    Delete block.py

--- block.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from linepy import *
-yinmo = LINE ()
-oepoll = OEPoll(yinmo)
-for contact in yinmo.getAllContactIds():
-    yinmo.blockContact(contact)
-    print("已封鎖1位好友")
