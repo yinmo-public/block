4b409508472ea64f9076dbe05d257e9e68e3adbc
yinmo-public <yinmo.contact@gmail.com>
2019-02-28 13:14:26 +0800

    Update block.py

--- block.py
+++ block.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from linepy import *
 yinmo = LINE()
-oepoll = OEPoll(yinmo)
 int1 = len(yinmo.getAllContactIds())
 if int1 == 0:
     print("您沒有可以封鎖的好友")
@@ -10,4 +9,3 @@ else:
         print("封鎖好友 " + yinmo.getContact(contact).displayName)
         yinmo.blockContact(contact)
     print("\n您封鎖" + str(int1) + "位好友")
-    
